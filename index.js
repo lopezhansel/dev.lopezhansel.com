7855093bc602299afa69d23066970524e142013c
Hansel Lopez <lopezhansel@gmail.com>
2018-02-14 10:25:48 -0700

    Adding express to serve webiste

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html')
+})
+
+app.listen(3333, () => console.log('Example app listening on port 3000!'))
