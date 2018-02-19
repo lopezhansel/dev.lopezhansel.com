e13f100593942e361bc3c9569f492c3cf13fd8bc
Hansel Lopez <lopezhansel@gmail.com>
2018-02-18 23:46:17 -0700

    Adding perliminary gallery

--- public/web/js/main.js
+++ public/web/js/main.js
@@ -1,4 +1,11 @@
 $(document).ready(() => {
     console.log(`There isn't much to this website`)
     $(".you-is-important").fitText(0.8);
+
+    $('.gallery').slick({
+        slidesToShow: 3,
+        autoplay: true,
+ 
+    });
+
 })
