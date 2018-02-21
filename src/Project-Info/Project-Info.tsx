0da4880b1cea918f27f15a304eea45bfdab0f8c8
Hansel Lopez <lopezhansel@gmail.com>
2018-02-20 22:48:49 -0700

    Fetching and rendering markdown

--- src/Project-Info/Project-Info.tsx
+++ src/Project-Info/Project-Info.tsx
@@ -1,17 +1,37 @@
 import * as React from 'react';
 import { Project } from '../portfolio/project';
 import './Project-Info.css';
+import axios from 'axios';
+import { Markdown } from '../Markdown/Markdown';
+
+function fetchMarkdown(url: string) {
+  return axios.get(url).then(d => d.data);
+}
+
+const input = '### fetching description..';
 
 class ProjectInfo extends React.Component {
   props: { selectedProject: Project };
+  state = { markdown: input };
+
+  getMarkDown() {
+    let { markdownUrl } = this.props.selectedProject;
+    fetchMarkdown(markdownUrl)
+      .then(s => {
+        this.state.markdown = s;
+        this.setState(this.state);
+      });
+  }
 
   componentDidMount() {
     setupSlick();
+    this.getMarkDown();
   }
 
   render() {
     const { name, company, description, gallery, stack, hasGallery, links } = this.props.selectedProject;
     const noContent = <p>Content is being worked on. Please come back later.</p>;
+
     return (
       <div className="row">
         <div className="project-info">
@@ -40,6 +60,7 @@ class ProjectInfo extends React.Component {
             <b>Links</b>
             <Links links={links} />
           </div>
+          <Markdown source={this.state.markdown} />
         </div>
       </div>
     );
