4b6037f4731eace947313f5bf504faa13221ad74
Hansel Lopez <lopezhansel@gmail.com>
2018-02-20 18:40:00 -0700

    Renaming navEnum to navSt.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -6,7 +6,7 @@ import ProjectList from '../Project-List/Project-List';
 import { projectData, Project } from '../portfolio/project';
 import * as classNames from 'classnames';
 
-enum navEnum {
+enum navSt {
   home,
   projectList,
   projectInfo,
@@ -14,12 +14,12 @@ enum navEnum {
 
 class App extends React.Component {
   state = {
-    appState: navEnum.home,
+    appState: navSt.home,
     projectData,
     selectedProject: projectData[0]
   };
 
-  setNavState(state: navEnum) {
+  setNavState(state: navSt) {
     return () => {
       this.state.appState = state;
       this.setState(this.state);
@@ -28,18 +28,18 @@ class App extends React.Component {
 
   onProjectSelect = (selectedProject: Project) => {
     this.state.selectedProject = selectedProject;
-    this.state.appState = navEnum.projectInfo;
+    this.state.appState = navSt.projectInfo;
     this.setState(this.state);
   }
 
-  isActive(check: navEnum) {
+  isActive(check: navSt) {
     switch (check) {
-      case navEnum.home:
-        return this.state.appState === navEnum.home;
-      case navEnum.projectList:
-        return this.state.appState === navEnum.projectList;
-      case navEnum.projectInfo:
-        return this.state.appState === navEnum.projectInfo;
+      case navSt.home:
+        return this.state.appState === navSt.home;
+      case navSt.projectList:
+        return this.state.appState === navSt.projectList;
+      case navSt.projectInfo:
+        return this.state.appState === navSt.projectInfo;
       default:
         return false;
     }
@@ -47,21 +47,20 @@ class App extends React.Component {
 
   render() {
     const { projectData: ProjectJson, selectedProject } = this.state;
-    const goHome = this.setNavState(navEnum.home);
-    const gotoProjectList = this.setNavState(navEnum.projectList);
+    const goHome = this.setNavState(navSt.home);
+    const gotoProjectList = this.setNavState(navSt.projectList);
 
     return (
       <div style={{ height: '100%' }}>
         <br />
         <div className="container">
-          <button className={classNames({ 'overwrite': true, 'active': this.isActive(navEnum.home) })} onClick={goHome}>Home </button>
-          <button className={classNames({ 'overwrite': true, 'active': this.isActive(navEnum.projectList) })} onClick={gotoProjectList}>Projects </button>
+          <button className={classNames('overwrite', { 'active': this.isActive(navSt.home) })} onClick={goHome}>Home </button>
+          <button className={classNames('overwrite', { 'active': this.isActive(navSt.projectList) })} onClick={gotoProjectList}>Projects </button>
         </div>
-        {this.isActive(navEnum.home) && <Home />}
-        {this.isActive(navEnum.projectInfo) && <Container><ProjectInfo selectedProject={selectedProject} /> </Container>}
-        {this.isActive(navEnum.projectList) && <Container><ProjectList projectData={ProjectJson} selectProject={this.onProjectSelect} /></Container>}
+        {this.isActive(navSt.home) && <Home />}
+        {this.isActive(navSt.projectInfo) && <Container><ProjectInfo selectedProject={selectedProject} /> </Container>}
+        {this.isActive(navSt.projectList) && <Container><ProjectList projectData={ProjectJson} selectProject={this.onProjectSelect} /></Container>}
       </div >
-
     );
   }
 }
